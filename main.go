6f52c8aaa9adee1342ac023bb192c61b628cbfa1
kostmetallist <kostmetallist@gmail.com>
2020-07-26 18:56:28 +0300

    Change port configuration to env var

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
 
+	portEnvVar := os.Getenv("PORT")
+	if portEnvVar == "" {
+		fmt.Println("PORT environment variable hasn't been set, aborting...")
+		return
+	}
+
+	portNumber, err := strconv.Atoi(portEnvVar)
+	if err != nil {
+		panic(err)
+	}
+
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	portNumber := 8008
 	fmt.Println(fmt.Sprintf("Launching on port %d...", portNumber))
-	err := http.ListenAndServe(fmt.Sprintf(":%d", portNumber), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", portNumber), nil)
 
 	if err != nil {
 		panic(err)
