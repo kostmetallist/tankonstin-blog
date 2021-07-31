04bbd23c7ceb6b4914028841fd6d52d696f89fb7
kostmetallist <kostmetallist@gmail.com>
2021-07-31 22:46:29 +0300

    Enable HTML templated endpoint

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -21,21 +22,22 @@ func main() {
 		panic(err)
 	}
 
-	ginEngine := gin.Default()
-	ginEngine.GET("/ping", func (c *gin.Context) {
+	router := gin.Default()
+
+	router.GET("/ping", func (c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H {
 			"result": "pong",
 		})
 	})
 
-	fmt.Println(fmt.Sprintf("Launching on port %d...", portNumber))
-	err = ginEngine.Run(fmt.Sprintf(":%d", portNumber))
+	router.LoadHTMLGlob("public/templates/*")
+	router.GET("/timer", func (c *gin.Context) {
+		c.HTML(http.StatusOK, "timer.tmpl", gin.H {
+			"time": time.Now().UTC().Format(time.RFC822),
+		})
+	})
 
-	// fileServer := http.FileServer(http.Dir("./static"))
-	// http.Handle("/", fileServer)
-	// err = http.ListenAndServe(fmt.Sprintf(":%d", portNumber), nil)
-
-	if err != nil {
+	if err = router.Run(fmt.Sprintf(":%d", portNumber)); err != nil {
 		panic(err)
 	}
 }
